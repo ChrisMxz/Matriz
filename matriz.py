75dde8b7ed9b39503cc03e0d2d36a0ea9c07d922
Chris <chris@math.com>
2021-05-26 12:42:28 -0500

    Metodo suma agregado metodo insertar y ver matrices

--- matriz.py
+++ matriz.py
@@ -1,59 +1,203 @@
+from ast import parse
 import os
+from numpy.lib.function_base import append
 import pyfiglet
 from colorama import Fore, Style
 from colorama import init
 from sympy import *
 import sympy as sy
-#Para imprimir
-from sympy.interactive.printing import init_printing
-init_printing(use_unicode=False, wrap_line=False)
-from sympy.matrices import Matrix, eye, zeros, ones, diag, GramSchmidt
+import numpy as np 
+from typing import OrderedDict
+from sympy.interactive.printing import NO_GLOBAL
+
+from sympy.simplify.fu import L
 
 
-#Funcion crea el banner de presentacion
-def banner():
-    ascii_banner = pyfiglet.figlet_format("Matriz")
-    print(Fore.GREEN + ascii_banner)
-    print(Fore.WHITE+"Elija la una opcion")
-    print(Fore.GREEN)
-    print("[1] Define una matriz")
-    print("[2] Lista de matrices guardadas")
-    print(Fore.YELLOW)
-    print("\n[S] Salir | [L] Limpiar pantalla")
-    print(Fore.WHITE)
+
+def lineinput(comando):
+    #convirtiendo a string linea ingresada por el usuario
+    line=str(comando)
+    #Partiendo comando en una lista 
+    lines=line.split()
+
+    return lines
 
 class Switcher(object):
-    def numero_del_metodo(self, argument):
-        """Dispatch method"""
-        nombre_metodo = 'metodo_' + str(argument)
-        metodo = getattr(self, nombre_metodo, lambda: "Opcion no valida")
-        # Call the method as we return it
-        metodo()
+    #Matrices creadas o guardadas
+    mtrxlist=np.array([])
+    mtxans=None
     
-    def metodo_1(self):
-        M1=Matrix([[1, 2, 3], [-2, 0, 4]])
-        print(M1)
+    #Agrega una variable a la lista de variables
+    @classmethod
+    def addmtx(cls,mtx):
+        nombre=input("Nombre de la matriz: ")
+        #diccionario
+        dato = {'id':nombre,'matriz':mtx}
+        cls.mtrxlist=np.append(cls.mtrxlist,dato)
+        print(Fore.GREEN+"Matriz almacenada")
+        print(Fore.WHITE)
+    
+    #Agrega una variable ans (resultadio de una operacion)
+    @classmethod
+    def addans(cls,mtx):
+        busca=False
+        dato=None
+        for x in cls.mtrxlist:
+            if x['id']=='ans':
+                busca=True
+                dato=x
+        if busca==True:
+            x['matriz']=mtx
+            print(Fore.GREEN)
+            print(x['matriz'])
+            print(Fore.WHITE)
+        else:
+            dato = {'id':'ans','matriz':mtx}
+            cls.mtrxlist=np.append(cls.mtrxlist,dato)
+            res=cls.busqueda('ans')
+            print(Fore.GREEN)
+            print(res['matriz'])
+            print(Fore.WHITE)
+    
+    #Busca un elemento en la lista de variables
+    @classmethod
+    def busqueda(cls,id):
+        for x in cls.mtrxlist:
+            if x['id']==id:
+                return x
 
-    #Funcion para limpiar pantalla
-    def metodo_L(self):
+    
+    #Muestra el menu principal
+    def menu(self):
+        salir=False
+        res=False
+        #Lista de metodos
+
+        self.banner()
+        while not salir:   
+            respuesta=input('\n-> ')
+            salir=respuesta=="s"
+
+            data=lineinput(respuesta)
+            mtd=""
+            #Obteniendo metodo
+            if len(data)==3:
+                mtd=data[1]
+                if mtd=='+':
+                    mtd='suma'
+                elif mtd=='-':
+                    mtd='resta'
+                else:
+                    mtd=None
+
+            elif len(data)>0:
+                mtd=data[0]
+            else:
+                mtd=None
+
+            nombre_metodo = 'metodo_' + str(mtd)
+            metodo = getattr(self, nombre_metodo, lambda default: "No encontrado")
+            # Call the method as we return it
+            metodo(data)
+    
+    #Crea una matriz
+    def metodo_1(self,argumento=None):
+        """Crear una matriz"""
+        f=int(input("Numero de filas: "))
+        c=int(input("Numero de Columnas: "))
+        Mtx=np.zeros((f,c))
+        for k in range(f):
+            for l in range(c):
+                print("["+str(k+1)+","+str(l+1)+"]",end="")
+                Mtx[k][l]=input("->")
+
+        self.addmtx(Mtx)
+
+    #limpia pantalla
+    def metodo_l(self,argumento=None):
+        m=Switcher()
         #Para linux
         if os.name == "posix":
             os.system ("clear")
-            banner()
+            self.banner()
         elif os.name == "ce" or os.name == "nt" or os.name == "dos":
             #Para Windows
             os.system ("cls")
-            banner()
+            self.banner()
 
-salir=False
+    def banner(self):
+        #banner
+        print('-'*30)
+        ascii_banner = pyfiglet.figlet_format("Matriz")
+        print(Fore.GREEN + ascii_banner)
 
-banner()
-while not salir:
-    mtd=Switcher()#Instancia de la clase Switcher
-    opc=input("-> ")
-    mtd.numero_del_metodo(opc)
-    
-    if opc=="s":
-        salir=True
-    elif opc=="S":
-        salir=True
+        #Lista de Opciones
+        menu=OrderedDict(
+        [
+            ('1',"Crear"),
+            ('2',"Editar"),
+            ('3',"Ver"),
+            ('l',"limpiar"),
+            ('s',"salir")
+            ]
+        )
+        #Muestra el menu
+        print(Fore.YELLOW)
+        for opcion, funcion in menu.items():
+            msj_final='[{}]{} '.format(opcion,funcion)
+            print(msj_final,end="")
+        print(Fore.WHITE)
+        
+    def editar():
+        """Editar Matriz"""
+        print("editando")
+        
+    def metodo_3(self,mtx):
+        """Matrices guardadas"""
+        print("Matrices guardadas")
+        for obj in self.mtrxlist:
+            name=Fore.YELLOW+'Id: {}'.format(obj['id'])+Fore.BLUE
+            m='\n{}'.format(obj['matriz'])+Fore.WHITE+'\n'
+            print(name+m)
+        
+
+    def metodo_suma(self,mtx):
+        if len(self.mtrxlist)==0:
+            print("No hay ninguna matriz guardada")
+        else:
+            msj="Suma de matriz"
+            print('-'*len(msj))
+            print(msj)
+            #Obteniendo el nombre de las matrices
+            a=mtx[0]
+            b=mtx[2]
+            x1=None
+            x2=None
+            bandera1=False
+            bandera2=False
+
+            #Buscando en la lista de matrices guardadas 
+            for x in self.mtrxlist:
+                if x['id']==a:
+                    x1=x['matriz']
+                    bandera1=True
+                if x['id']==b:
+                    x2=x['matriz']
+                    bandera2=True
+           
+            if bandera1 and bandera2 == True:
+                res=x1+x2
+                self.addans(res)
+            elif bandera1==False:
+                print(Fore.RED+' Matriz ('+a+') no encontrada'+Fore.WHITE)
+            elif bandera2==False:
+                print(Fore.RED+' Matriz ('+b+') no encontrada'+Fore.WHITE)
+            
+    def default(self,argumento=None):
+        print("Ingrese una opcion valida")
+        
+
+
+if __name__=='__main__':
+    obj=Switcher()
+    obj.menu()
